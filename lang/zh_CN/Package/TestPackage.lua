c984ecba1bc696e89a007e0d243dfa6e8af9cc1e
LiuChang <liuchang@nfs.iscas.ac.cn>
2018-06-17 17:21:05 +0800

    Clear translation files.

---
+++ lang/zh_CN/Package/TestPackage.lua
@@ -0,0 +1,16 @@
+-- translation for Standard General Package
+
+return {
+	-- Test
+	["test"] = "测试",
+
+	["#sujiang"] = "金童",
+	["sujiang"] = "士兵(男)",
+	["&sujiang"] = "士兵",
+	["illustrator:sujiang"] = "木美人",
+	["#sujiangf"] = "玉女",
+	["sujiangf"] = "士兵(女)",
+	["&sujiangf"] = "士兵",
+	["illustrator:sujiangf"] = "木美人",
+}
+
